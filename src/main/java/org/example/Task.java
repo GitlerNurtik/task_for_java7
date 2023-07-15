896994ef62172fc83857f8ac740b54fad52d837f
medertynychbekov <meder.tynychbekov@gmail.com>
2023-07-15 17:02:40 +0300

    Initial commit

---
+++ src/main/java/org/example/Task.java
@@ -0,0 +1,106 @@
+package org.example;
+
+public class Task {
+
+    /**
+     * В качестве параметра приходит одно число, получить сумму всех чисел из заданной числы.
+     *
+     * @param a = 345;
+     * @return 12;
+     */
+    public int getSum(int a) {
+        return 0;
+    }
+
+    /**
+     * В качестве параметра приходит массив целых чисел.
+     * Вывести на консоль четвертый максимальный элемент.
+     *
+     * @param array = {5, 9, 4, 12, 20, -2};
+     *              Например: четвертый максимальный элемент это 5.
+     */
+    public void getArray(int[] array) {
+
+    }
+
+
+    /**
+     * В качестве параметра приходит текст,
+     * ваша задача вытащить самую последнюю букву в алфавитном порядке.
+     *
+     * @param text = "Школа"
+     * @return "Ш"
+     */
+    public char getLastSymbol(String text) {
+
+        return 0;
+    }
+
+
+    /**
+     * В качестве параметра приходит массив строк, ваша задача вернуть самую длинную строку.
+     *
+     * @param wordArray = {delphin, dream, Los_Angeles}
+     * @return "Los_Angeles"
+     */
+    public String getWord(String[] wordArray) {
+        return "";
+    }
+
+
+    /**
+     * В качестве параметра приходит массив строк,
+     * ваша задача вернуть строку, которая повторяется наибольшее количество раз.
+     *
+     * @param text {ручка, телефон, книга, телефон, ручка, телефон}
+     * @return телефон
+     */
+    public String getMaxDuplicateWord(String[] text) {
+        return "";
+    }
+
+
+    /**
+     * @param a = 8;
+     * @return array = {1,2,3,4,5,6,7,8}
+     */
+    public String[] buildArray(int a) {
+        return new String[0];
+    }
+
+
+    /**
+     * В качестве параметра метода приходит текст,
+     * ваша задача, если встречается пробел,
+     * то следующее слово опустить на новую строку.
+     *
+     * @param text {Hello world, my name is Mr.Prospector}
+     *             Вывод на консоль: hello
+     *             world,
+     *             my
+     *             name
+     *             is
+     *             Mr.Prospector
+     */
+    public void method(String text) {
+
+    }
+
+
+    /**
+     * Учитывая массив целых чисел num и целочисленное целевое значение,
+     * верните индексы двух чисел таким образом, чтобы они складывались в целевое значение.\
+     * Входные данные: array = [2,7,11,15], target = 9
+     * Выход: [0,1]
+     * Пояснение: Поскольку array[0] + array[1] == 9, мы возвращаем [0, 1].
+     *
+     * @param array  = {2,7,11,15};
+     * @param target = 9;
+     * @return {0, 1}.
+     */
+    public int[] getSum(int[] array, int target) {
+        return new int[0];
+    }
+
+
+}
